40fb13f464e537f071111117917372a6e23d146e
johns2601 <johns2601@hotmail.com>
2014-05-25 05:52:11 -0300

    Se actualiza TDA recorrido
    Se actualiza TDA recorrido

--- include/recorrido.h
+++ include/recorrido.h
@@ -1,21 +1,27 @@
 #ifndef RECORRIDO_H_INCLUDED
 #define RECORRIDO_H_INCLUDED
-
 #include "lista.h"
+
+typedef struct {
+    unsigned char Red;
+    unsigned char Green;
+    unsigned char Blue;
+}RGB;
+
 
 class Recorrido{
     private:
-        Lista <char>* Caminos1;
-        Lista <char>* Bifurcaciones1;
+        Lista <char>* caminos1;
+        Lista <char>* bifurcaciones1;
         int cantBifurcaciones;
         int cantUniRecorridas;
         int cantGiros;
-        int color[3];
+        RGB color;
 
     public:
 
         //PRE
-        //POST:
+        //POST: inicializa los punteros en NULL, las cantidades en cero y el color en (0;0;0)
         Recorrido();
         //PRE
         //POST:
@@ -25,17 +31,16 @@ class Recorrido{
         //POST:Agrega un nodo Bifurcacion en el camino y en la lista de bifurcaciones y que ademas sume uno al this->cantidadBifurcaciones
         void agregarBifurcacion();
         //PRE
+        //POST: Setea el color del recorrido que esta trazando.
+
+/***LOS SIGUIENTES METODOS TIENE QUE SER CORREGIDOS*/
+        void setColor(char);
+        //PRE
         //POST:
         void agregarGiro();
         //PRE
-        //POST: Setea el color del recorrido que esta trazando.
-        void setColor();
-        //PRE
         //POST:
         void avanzar ();
-        //PRE
-        //POST:
-        void retroceder();
         //PRE
         //POST:
         void leventarElemento();
--- src/recorrido.cpp
+++ src/recorrido.cpp
@@ -1,1 +1,15 @@
 #include "recorrido.h"
+
+//Contructor
+Recorrido::Recorrido(){
+    caminos1 = NULL;
+    bifurcaciones1 = NULL;
+    cantBifurcaciones = 0;
+    cantGiros = 0;
+    cantUniRecorridas = 0;
+    color.Blue=0;
+    color.Green=0;
+    color.Red=0;
+}
+
+
