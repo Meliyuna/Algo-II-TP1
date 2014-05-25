54a82fb5505d21ba1627ad93203694f096cf8540
Melisa Romero <melisa@meli-PC.(none)>
2014-05-25 04:27:01 -0300

    avisen si lo ven

---
+++ include/laberinto.h
@@ -0,0 +1,44 @@
+#ifndef LABERINTO_H_H
+#define LABERINTO_H_H
+
+
+class Laberinto.h
+{
+    public:
+        //Constructor
+        //PRE
+        //POST
+        Laberinto.h();
+
+        //Actualizar Coordenadas
+        //PRE
+        //POST
+        void actualizarCoordenadas();
+
+        //Lectura
+        //PRE: Lee un archivo de texto, determinado por el usuario
+        //POST: Guarda la informacion en "Camino" o "Mochila"  segun corresponda
+        void lectura();
+
+        //Buscar en Mochila
+        //PRE: Recibe un parametro para buscar en "Mochila"
+        //POST: Indica si el elemento se encuentra o no
+        bool buscarEnMochila(string nombre);
+
+        //Destructor
+        //PRE
+        //POST
+        virtual ~Laberinto.h();
+
+    private:
+        int ancho;
+        int largo;
+        Lista<*Tda Recorridos>* recorrido;
+        Lista<*Tda Mochila>* mochila;
+};
+
+#endif // LABERINTO_H_H
+
+
+
+
---
+++ src/laberinto.cpp
@@ -0,0 +1,33 @@
+#include "Laberinto.h"
+#include "Nodo.h"
+
+Lista<Mochila.h>* mochila;
+
+Laberinto::Laberinto()
+{
+    //ctor
+}
+
+bool Laberinto::buscarEnMochila (string nombre)
+{
+    //uso busqueda secuencial
+    int encontrado=0;
+
+    string comparar=mochila->primero->getNombre;
+    while ((mochila->tieneSiguiente())&&(!encontrado))
+    {
+        if (comparar!=nombre)
+        {
+            mochila->avanzarCursor();
+            comparar=mochila->cursor->getNombre();
+        }
+        else
+            encontrado++;
+    }
+    return encontrado;
+}
+
+Laberinto::~Laberinto()
+{
+    //dtor
+}
