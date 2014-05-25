2a7537d882509534a4040f6d74b368d9455ed2d6
johns2601 <johns2601@hotmail.com>
2014-05-25 14:52:01 -0300

    Se actualiza recorrido
    Problemas en la copilacion de un fuck string

--- include/recorrido.h
+++ include/recorrido.h
@@ -1,6 +1,8 @@
 #ifndef RECORRIDO_H_INCLUDED
 #define RECORRIDO_H_INCLUDED
+#include <string>
 #include "lista.h"
+
 
 typedef struct {
     unsigned char Red;
@@ -26,15 +28,20 @@ class Recorrido{
         //PRE
         //POST:
         ~Recorrido();
+        //PRE
+        //POST: Setea el color del recorrido que esta trazando.
+        void setColor(RGB colorCamino);
+        //PRE: Se agrega el comando para que determine si crea el camino en memoria
+        //POST:
+        void agregarCamino(string comando);
 
+/***LOS SIGUIENTES METODOS TIENE QUE SER CORREGIDOS*/
         //PRE
         //POST:Agrega un nodo Bifurcacion en el camino y en la lista de bifurcaciones y que ademas sume uno al this->cantidadBifurcaciones
         void agregarBifurcacion();
         //PRE
         //POST: Setea el color del recorrido que esta trazando.
-
-/***LOS SIGUIENTES METODOS TIENE QUE SER CORREGIDOS*/
-        void setColor(char);
+        void setColor(RGB colorCamino);
         //PRE
         //POST:
         void agregarGiro();
--- src/recorrido.cpp
+++ src/recorrido.cpp
@@ -11,5 +11,27 @@ Recorrido::Recorrido(){
     color.Green=0;
     color.Red=0;
 }
+//Selecciona el color del recorrido
+void Recorrido::setColor(RGB colorCamino){
+    color.Blue = colorCamino.Blue;
+    color.Red  = colorCamino.Red;
+    color.Green= colorCamino.Green;
+
+}
+
+void Recorrido::agregarCamino(string comando){
+
+    switch comando[0]{
+        case G:
+                break;
+        case A:
+                break;
+        case R:
+                break;
+
+    }
+
+
+}
 
 
